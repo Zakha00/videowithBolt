f3ffff616ab432079dd6fcae99beb2b94beb8d00
Zakha00 <56904483+Zakha00@users.noreply.github.com>
2026-03-26 22:40:15 +0600

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,23 @@ from aiogram.types import Message, FSInputFile
 from aiogram.filters import CommandStart
 import yt_dlp
 
+import threading
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import os
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'Bot is running')
+
+def run_web():
+    port = int(os.environ.get("PORT", 10000))
+    server = HTTPServer(("0.0.0.0", port), Handler)
+    server.serve_forever()
+
+threading.Thread(target=run_web).start()
+
 TOKEN = "7992634454:AAGYAg70kCp5ye79FeuerTAnsgMe5Gg4zZY"
 
 bot = Bot(token=TOKEN)
